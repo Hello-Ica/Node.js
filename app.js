e16e4611db081b07c422b3d02e5d418335868a56
Ica <herrerajenica21@gmail.com>
2026-08-17 18:48:32 +0800

    lesson 1 and 2

---
+++ app.js
@@ -0,0 +1,12 @@
+// Modules
+const secret = 'SUPER SECRET'
+const john = 'John'
+const peter = 'Peter'
+
+const sayHi = (name) => {
+  console.log(`Hello there ${name}`)
+}
+
+sayHi('Susan')
+sayHi(john)
+sayHi(peter)
