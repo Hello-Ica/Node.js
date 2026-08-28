69ace74128c37524d5bcf21b987d6298a8f72ed8
Ica <herrerajenica21@gmail.com>
2026-08-28 18:23:19 +0800

    learned alternative syntax

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-// Modules
-const secret = 'SUPER SECRET'
-const john = 'John'
-const peter = 'Peter'
 
-const sayHi = (name) => {
-  console.log(`Hello there ${name}`)
-}
+// CommonJS, every file is a module (by default)
+// Modules Encapaulated Code (only sgare minimum)
 
+
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+console.log(sayHi)
+const data = require('./6-alternative-flavor');
+require('./7-mind-grenade')
 sayHi('Susan')
-sayHi(john)
-sayHi(peter)
+sayHi(names.john)
+sayHi(names.peter)
