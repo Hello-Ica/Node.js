b9455f73c29ce689c43aa3b2d54af0103991d3fc
Ica <herrerajenica21@gmail.com>
2026-09-02 17:35:05 +0800

    file sync

---
+++ app.js
@@ -0,0 +1,9 @@
+const { readFile, write} = require('fs')
+
+readFile('./content/first.txt', 'utf8', (err, result) => {
+  if (err) {
+    console.log(err);
+    return
+  }
+  console.log(result);
+})
