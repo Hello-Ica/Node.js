f2167404a4887f0c0c17232e4e32f0429c4f7ab2
Ica <herrerajenica21@gmail.com>
2026-09-10 21:26:34 +0800

    http and npm

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-const http = require('http');
+// npm - global command, comes with node
+// npm --version
 
-const server = http.createServer((req, res) => {
-res.write('Welcome to our home page')
-res.end()
-})
+// local dependency - use it only in this particular project
+// npm i <packageName>
 
-server.listen(5000)
+//gloabal dependency -use it in any project
+// npm install -g <packageName>
+// sudo npm install -g <packageName> (mac)
+
+//packaye.json - manifest file (stores important info about project/package)
+//manual approach (create package.json in the root, create properties etc)
+//npm init (step by step, press enter to skip)
+//npm init -y (everything default)
